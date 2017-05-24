44d5761efe200f386b04257b7da9978abbb5b0c1
Tom Kent <tkent198@gmail.com>
2017-05-24 14:54:37 +0100

    tkmac

--- offlineQ.py
+++ offlineQ.py
@@ -1,9 +1,9 @@
 #######################################################################
 # Investigating computation and structure of model error and candidate Q matrices
-# (T. Kent: mmtk@leeds.ac.uk)
+# (T. Kent: tkent198@gmail.com)
 #######################################################################
 '''
-    
+    Comparing time-dep. and 2 static Q matrices
     
     '''
 import math as m
@@ -13,6 +13,8 @@ from parameters import *
 import os
 import errno
 
+
+##################################################################
 
 ##################################################################
 
@@ -71,7 +73,10 @@ for T in time_vec[1:]:
     Xdev_tr[:,:,T] = X[:,:,T] - X_tr[:,:,T] # fc deviations from truth
     Q_array[:,:,T] = np.dot(Xdev_tr[:,:,T], Xdev_tr[:,:,T].T)/(n_ens-1)
 
-T = time_vec[30]
+Xdev_tr_new = Xdev_tr.reshape(n_d,n_ens*t_an)
+print np.shape(Xdev_tr_new)
+
+T = time_vec[24]
 
 ## time-dep Q matrix (at time T)
 QT = Q_array[:,:,T]
@@ -82,23 +87,23 @@ Q_ave = Q_array[:,:,:].mean(axis=2)
 print np.shape(Q_ave)
 Q = Q_ave
 
+Qnew = np.dot(Xdev_tr_new, Xdev_tr_new.T)/(n_ens-1)
+
 q = np.random.multivariate_normal(np.zeros(600), Q, n_ens)
 q = q.T
 
 qt = np.random.multivariate_normal(np.zeros(600), QT, n_ens)
 qt = qt.T
 
+qnew = np.random.multivariate_normal(np.zeros(600), Qnew, n_ens)
+qnew = qnew.T
+
 print 'q shape: ', np.shape(q)
 print ' '
-print 'max Q static value: ', np.max(Q)
-print 'max Q t-dep. value: ', np.max(QT)
-print ''
-print 'min Q static value: ', np.min(Q)
-print 'min Q t-dep. value: ', np.min(QT)
 
 alph=0.2
 
-fig, axes = plt.subplots(2, 2, figsize=(10,10))
+fig, axes = plt.subplots(3, 2, figsize=(10,10))
 
 axes[0,0].plot(range(0,Nk_fc), q[0:200,0],'g',label='h',alpha=alph)
 axes[0,0].plot(range(0,Nk_fc), q[200:400,0],'r',label='u',alpha=alph)
@@ -106,27 +111,41 @@ axes[0,0].plot(range(0,Nk_fc), q[400:600,0],'b',label='r',alpha=alph)
 axes[0,0].plot(range(0,Nk_fc), q[0:200,1:],'g',alpha=alph)
 axes[0,0].plot(range(0,Nk_fc), q[200:400,1:],'r',alpha=alph)
 axes[0,0].plot(range(0,Nk_fc), q[400:600,1:],'b',alpha=alph)
-axes[0,0].set_ylim([-1.5,1.5])
+axes[0,0].set_ylim([-2,2])
 axes[0,0].set_xlabel('x')
 axes[0,0].set_title('samples from static Q')
 axes[0,0].legend(loc = 1)
 
-axes[1,0].plot(range(0,Nk_fc), qt[0:200,0],'g',label='h',alpha=alph)
-axes[1,0].plot(range(0,Nk_fc), qt[200:400,0],'r',label='u',alpha=alph)
-axes[1,0].plot(range(0,Nk_fc), qt[400:600,0],'b',label='r',alpha=alph)
-axes[1,0].plot(range(0,Nk_fc), qt[0:200,1:],'g',alpha=alph)
-axes[1,0].plot(range(0,Nk_fc), qt[200:400,1:],'r',alpha=alph)
-axes[1,0].plot(range(0,Nk_fc), qt[400:600,1:],'b',alpha=alph)
-axes[1,0].set_ylim([-1.5,1.5])
+axes[2,0].plot(range(0,Nk_fc), qt[0:200,0],'g',label='h',alpha=alph)
+axes[2,0].plot(range(0,Nk_fc), qt[200:400,0],'r',label='u',alpha=alph)
+axes[2,0].plot(range(0,Nk_fc), qt[400:600,0],'b',label='r',alpha=alph)
+axes[2,0].plot(range(0,Nk_fc), qt[0:200,1:],'g',alpha=alph)
+axes[2,0].plot(range(0,Nk_fc), qt[200:400,1:],'r',alpha=alph)
+axes[2,0].plot(range(0,Nk_fc), qt[400:600,1:],'b',alpha=alph)
+axes[2,0].set_ylim([-2,2])
+axes[2,0].set_xlabel('x')
+axes[2,0].set_title('samples from time-dep. Q')
+axes[2,0].legend(loc = 1)
+
+axes[1,0].plot(range(0,Nk_fc), qnew[0:200,0],'g',label='h',alpha=alph)
+axes[1,0].plot(range(0,Nk_fc), qnew[200:400,0],'r',label='u',alpha=alph)
+axes[1,0].plot(range(0,Nk_fc), qnew[400:600,0],'b',label='r',alpha=alph)
+axes[1,0].plot(range(0,Nk_fc), qnew[0:200,1:],'g',alpha=alph)
+axes[1,0].plot(range(0,Nk_fc), qnew[200:400,1:],'r',alpha=alph)
+axes[1,0].plot(range(0,Nk_fc), qnew[400:600,1:],'b',alpha=alph)
+axes[1,0].set_ylim([-2,2])
 axes[1,0].set_xlabel('x')
-axes[1,0].set_title('samples from time-dep. Q')
+axes[1,0].set_title('samples from new static Q')
 axes[1,0].legend(loc = 1)
 
 axes[0,1].imshow(Q,cmap=plt.cm.RdBu,vmin=-0.05, vmax=0.05)
 axes[0,1].set_title('static Q')
 
-axes[1,1].imshow(QT,cmap=plt.cm.RdBu,vmin=-0.05, vmax=0.05)
-axes[1,1].set_title('time-dep. Q')
+axes[2,1].imshow(QT,cmap=plt.cm.RdBu,vmin=-0.05, vmax=0.05)
+axes[2,1].set_title('time-dep. Q')
+
+axes[1,1].imshow(Qnew,cmap=plt.cm.RdBu,vmin=-0.05, vmax=0.05)
+axes[1,1].set_title('new static Q')
 
 plt.show()
 
